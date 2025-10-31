cf5ee7703d8b36d6269d37793de4d2381af19cb1
BerJam <moletaber64@gmail.com>
2025-10-31 16:18:41 -0300

    Retira o overrite do global excepition handler

--- src/main/java/com/bjam/guardian/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bjam/guardian/exception/GlobalExceptionHandler.java
@@ -1,20 +1,22 @@
 package com.bjam.guardian.exception;
 
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // captura erros de @Valid
-    @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
                                                                   HttpStatus status,
