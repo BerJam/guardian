aacf7cce238d7f4027a7a917dd118ca297f2197e
BerJam <moletaber64@gmail.com>
2025-11-08 08:42:32 -0300

    Ajustes no alert repository

--- src/main/java/com/bjam/guardian/repository/AlertRepository.java
+++ src/main/java/com/bjam/guardian/repository/AlertRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bjam.guardian.model.Alert;
-import com.bjam.guardian.model.Transaction;
 
 @Repository
 public interface AlertRepository extends JpaRepository<Alert, Long> {
-    List<Alert> findByRead(boolean read);
-    List<Alert> findByRelatedTransaction(Transaction tx);
-    List<Alert> findByRelatedTransactionId(Long txId);
+
+    // Antes: List<Alert> findByRead(boolean read);
+    // Agora: pesquisa pelo nome da propriedade 'isRead' -> use 'IsRead' no método
+    List<Alert> findByIsRead(boolean isRead);
+
+    // outros métodos customizados, se houver
 }
